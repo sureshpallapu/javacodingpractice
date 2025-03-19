56f98fd72e4f8b7246c067ad03c1c008dafbb8e5
sureshpallapu <pallapusuresh2000@gmail.com>
2025-03-19 15:07:40 +0530

    new

---
+++ Pentagon/src/Exception/Agelessthantomarriage.java
@@ -0,0 +1,10 @@
+package Exception;
+
+public class Agelessthantomarriage extends Exception{
+	 Agelessthantomarriage(){
+		 System.out.println(" ages are less ,So not eligible to marriage ");
+	 }
+	 Agelessthantomarriage(String s){
+		 System.out.println(s);
+	 }
+}
