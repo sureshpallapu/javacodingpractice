c10613d62c909686cbf8d755262e4c079deaafe7
sureshpallapu <pallapusuresh2000@gmail.com>
2025-03-22 19:22:38 +0530

    new

---
+++ Pentagon/src/in/CollectionFramework/List/ArrayList/ArrayListSizeExample.java
@@ -0,0 +1,11 @@
+package in.CollectionFramework.List.ArrayList;
+
+import java.util.*;
+
+public class ArrayListSizeExample {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>(Arrays.asList("Apple", "Banana", "Cherry"));
+
+        System.out.println("Size of ArrayList: " + list.size());
+    }
+}
