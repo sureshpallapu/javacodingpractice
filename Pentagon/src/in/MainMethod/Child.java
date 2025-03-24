f20e512b9e1d7d3e6d5805d8545aab82dfc7fbc7
sureshpallapu <pallapusuresh2000@gmail.com>
2025-03-24 19:49:41 +0530

    new

---
+++ Pentagon/src/in/MainMethod/Child.java
@@ -0,0 +1,13 @@
+package in.MainMethod;
+
+class Parent {
+    public static void main(String[] args) {
+        System.out.println("Parent class main method");
+    }
+}
+
+ class Child extends Parent {
+    public static void main(String[] args) {
+        System.out.println("Child class main method");
+    }
+}
