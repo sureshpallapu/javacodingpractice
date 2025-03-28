d7f0f14393bdee5589c247cf998517bb8df75282
sureshpallapu <pallapusuresh2000@gmail.com>
2025-03-28 21:20:00 +0530

    new

--- Pentagon/src/Pentagon/leftangledtriangle.java
+++ Pentagon/src/Pentagon/leftangledtriangle.java
@@ -10,8 +10,7 @@ public class leftangledtriangle {
 		int n;
 		n = s.nextInt();
 		  for (int i = 1; i <= n; i++) {  
-	            for (int j = 1; j <=
-	            		n-i ; j++) { 
+	            for (int j = 1; j <=n-i ; j++) { 
 	                System.out.print("  ");
 	            }
 	            for (int k = 1; k <= i; k++) {
