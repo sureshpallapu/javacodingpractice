c3d0d1b157ade04d45e750f7402ff41c1029f87b
sureshpallapu <pallapusuresh2000@gmail.com>
2025-03-29 23:11:50 +0530

    new

---
+++ Pentagon/src/in/inheritance/CyclicInheritance.java
@@ -0,0 +1,15 @@
+package in.inheritance;
+//class A extends B{
+//	
+//}
+//class B extends A{
+//	
+//}
+
+public class CyclicInheritance {
+	
+public static void main(String[] args) {
+//	A a=new B();
+	
+}
+}
