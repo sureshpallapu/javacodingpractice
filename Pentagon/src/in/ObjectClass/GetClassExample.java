c764be46e328ce149b41cf757554b2b3b4665ac5
sureshpallapu <pallapusuresh2000@gmail.com>
2025-04-08 22:59:24 +0530

    new

---
+++ Pentagon/src/in/ObjectClass/GetClassExample.java
@@ -0,0 +1,8 @@
+package in.ObjectClass;
+
+class GetClassExample {
+	public static void main(String[] args) {
+		GetClassExample t = new GetClassExample();
+		System.out.println(t.getClass().getName()); // Output: Test
+	}
+}
