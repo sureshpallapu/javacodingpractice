319ed020c5e17029611ce4307661cd8b4aed46fe
sureshpallapu <pallapusuresh2000@gmail.com>
2025-04-09 22:12:12 +0530

    new

--- Pentagon/src/in/abstraction/AbstractExample.java
+++ Pentagon/src/in/abstraction/AbstractExample.java
@@ -1,28 +1,28 @@
 package in.abstraction;
 
-abstract class Shape {
-    abstract void draw();
-}
-
-class Circle extends Shape {
-    void draw() {
-        System.out.println("Drawing a Circle");
-    }
-}
-
-class Square extends Shape {
-    void draw() {
-        System.out.println("Drawing a Square");
-    }
-}
-
-public class AbstractExample {
-    public static void main(String[] args) {
-        Shape c = new Circle();
-        Shape s = new Square();
-        
-        c.draw();
-        s.draw();
-    }
-}
+//abstract class Shape {
+//    abstract void draw();
+//}
+//
+//class Circle extends Shape {
+//    void draw() {
+//        System.out.println("Drawing a Circle");
+//    }
+//}
+//
+//class Square extends Shape {
+//    void draw() {
+//        System.out.println("Drawing a Square");
+//    }
+//}
+//
+//public class AbstractExample {
+//    public static void main(String[] args) {
+//        Shape c = new Circle();
+//        Shape s = new Square();
+//        
+//        c.draw();
+//        s.draw();
+//    }
+//}
 
