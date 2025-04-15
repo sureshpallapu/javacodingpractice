f489d297d7c13834665367545eb7c113ddacde8d
sureshpallapu <pallapusuresh2000@gmail.com>
2025-04-15 20:15:50 +0530

    new

---
+++ Pentagon/src/in/CollectionFramework/List/ListExample.java
@@ -0,0 +1,14 @@
+package in.CollectionFramework.List;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ListExample {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Cherry");
+        System.out.println(list); // Output: [Apple, Banana, Cherry]
+    }
+}
