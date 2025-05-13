e890eef449b18be91e6d043e91246c88604c32a3
sureshpallapu <pallapusuresh2000@gmail.com>
2025-05-13 12:26:36 +0530

    new

---
+++ Pentagon/src/in/Polymorphism/OperatorOverloading.java
@@ -0,0 +1,11 @@
+package in.Polymorphism;
+
+public class OperatorOverloading {
+	public static void main(String[] args) {
+		int a = 10, b = 20;
+		System.out.println(a + b); // Output: 30 (Addition)
+
+		String s1 = "Hello", s2 = "World";
+		System.out.println(s1 + " " + s2); // Output: Hello World (Concatenation)
+	}
+}
